9ce9d16644fd19fa0f619291fc4fb5143065a5f9
Too Bag <istoobag@gmail.com>
2017-06-03 16:28:52 +0800

    run
    service on check username value

--- AnguarApp/src/app/form/login/login.component.ts
+++ AnguarApp/src/app/form/login/login.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../core/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  service: AuthService;
 
-  constructor() { }
+  constructor() {
+    this.service = new AuthService();
+   }
 
   ngOnInit() { }
 
   onClick(username, password) {
     console.log('username:' + username + "\n\r" + "password:" + password);
+    console.log('auth result is: ' + this.service.loginWithCredentials(username, password));
   }
   
 }
